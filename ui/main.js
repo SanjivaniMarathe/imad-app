95b1bedb88567ef3e0445fc7fe630a65fe26692f
SanjivaniMarathe <30775772+SanjivaniMarathe@users.noreply.github.com>
2017-09-05 21:39:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+//change the text of the main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'Welcome !';
+
+//Move the image
+var img = document.getElementByID9'img');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+};
