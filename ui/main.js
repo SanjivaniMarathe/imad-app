585ec82b7228bbb0c6ae8840d539c4850a1fe2e9
SanjivaniMarathe <30775772+SanjivaniMarathe@users.noreply.github.com>
2017-09-05 22:13:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Welcome !';
 
 //Move the image
-var img = document.getElementByID('img');
+var img = document.getElementByID('madi');
 img.onclick = function() {
     img.style.marginleft = '300px';
 };
